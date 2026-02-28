400f11f8f67a96e396bfa8f3624e805936a9c03c
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-02-28 00:51:11 -0300

    up

--- eng-fluency-platform/frontend/nuxt.config.ts
+++ eng-fluency-platform/frontend/nuxt.config.ts
@@ -1,18 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  future: {
-    compatibilityVersion: 4,
-  },
-  pinia: {
-    storesDirs: ['./app/store/**']
-  },
   imports: {
     dirs: [
-      'app/store',
-      'app/composables',
+      'store',
+      'composables',
     ]
   },
   runtimeConfig: {
@@ -22,3 +16,4 @@ export default defineNuxtConfig({
     }
   }
 })
+
